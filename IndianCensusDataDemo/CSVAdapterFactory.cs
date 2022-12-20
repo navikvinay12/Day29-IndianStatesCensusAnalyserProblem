569635d952e62b0b4fed067747e950731abe5d3e
Vinay <navik.vinay12@gmail.com>
2022-12-20 22:54:37 +0530

    2 UCs each having 5 Tests on Census Analyser problem

---
+++ IndianCensusDataDemo/CSVAdapterFactory.cs
@@ -0,0 +1,31 @@
+﻿using IndiaCensusDataDemo;
+using IndianCensusDataDemo.DataDAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianCensusDataDemo
+{
+    public class CSVAdapterFactory
+    {
+        public Dictionary<string, CensusDataDAO> LoadCsvData(CensusAnalyser.Country country,string csvFilePath,string dataHeaders)
+        {
+            try
+            {
+                switch (country)
+                {
+                    case CensusAnalyser.Country.INDIA :
+                        return new IndianCensusAdapter().LoadCensusData(csvFilePath, dataHeaders);
+                    default:
+                        throw new CensusAnalyserException("No Such Country", CensusAnalyserException.ExceptionType.NO_SUCH_COUNTRY);
+                }
+            }
+            catch(CensusAnalyserException ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ IndianCensusDataDemo/DTO/CensusDTO.cs
@@ -0,0 +1,35 @@
+﻿using IndianCensusDataDemo.DataDAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianCensusDataDemo.DTO
+{
+    public class CensusDTO
+    {
+        public int serialNumber;
+        public string stateName;
+        public int tin;
+        public string stateCode;
+        public string state;
+        public long population;
+        public long area;
+        public long density;
+        public CensusDTO(StateCodeDataDao stateCodeDataDAO)
+        {
+            this.serialNumber=stateCodeDataDAO.serialNumber;
+            this.stateName=stateCodeDataDAO.stateName;
+            this.tin=stateCodeDataDAO.tin;
+            this.stateCode=stateCodeDataDAO.stateCode;
+        }
+        public CensusDTO(CensusDataDAO censusDataDAO)
+        {
+            this.state=censusDataDAO.state;
+            this.population=censusDataDAO.population;
+            this.area=censusDataDAO.area;
+            this.density=censusDataDAO.density;
+        }
+    }
+}
